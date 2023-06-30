b6c3832b035cdbb9e98ce24f7ab2e78cc582d7a8
Wilson Zimthamaha Bonkuru <wilsonzim566@gmail.com>
2023-06-30 19:05:27 +0100

    done

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,7 +7,6 @@ function Homepage() {
   const navigate = useNavigate();
   return (
     <div className="homepage">
-      <h1>Nigerian Society of Biochemistry Students, NSBS</h1>
       <h1>
         Quest for the crown of Mr/Miss face of NSBS 2022/2023 <br /> brought to you by
         Comr. Malaky Arinze NSBS D.O.S.
@@ -15,7 +14,7 @@ function Homepage() {
       <div className="queen">
         <img src={queen} alt="" />
         <h1>Miss Jenifer</h1>
-        <h3>Miss Face of NSBS 2021/2022</h3>
+        <h3>Face of NSBS 2021/2022</h3>
       </div>
       <hr />
       <p>
