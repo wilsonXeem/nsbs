40c7b1999bffced319463f42b2cf6e046d030118
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2024-07-15 11:46:51 +0100

    Update Contestants.js

--- src/components/Contestants.js
+++ src/components/Contestants.js
@@ -31,7 +31,7 @@ function Contestants() {
             <h3>{datas.fullname}</h3>
             <p>{datas.nickname}</p>
             <h1>
-              {data.votes} %
+              {data.vote} %
             </h1>
           </div>
         );
